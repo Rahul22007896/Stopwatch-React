b4a20047c4f7dcdf05673459b1078fcadab12c08
Rahul22007896 <rahulict518@gmail.com>
2024-07-10 14:53:01 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'Stopwatch-React',
   plugins: [react()],
+  base: '/Stopwatch-React/',
 })
